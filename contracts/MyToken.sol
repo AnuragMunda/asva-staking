0c1829fa68f50c785189e28403d87fc0702dae94
AnuragMunda <anurag.munda005@gmail.com>
2025-07-29 20:18:48 +0530

    add erc20 token contract

---
+++ contracts/MyToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("MyToken", "MTK") {
+        _mint(msg.sender, 100_000 * 1e18);
+    }
+
+    function mint(address _to, uint256 _amount) public {
+        _mint(_to, _amount);
+    }
+}
