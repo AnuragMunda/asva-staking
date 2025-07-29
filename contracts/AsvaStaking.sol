974503d89e7e47c3286df2c2cfa6d69a700f0325
AnuragMunda <anurag.munda005@gmail.com>
2025-07-29 19:21:40 +0530

    Basic contract setup

---
+++ contracts/AsvaStaking.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+/**
+ * @title AsvaStaking Smart Contract
+ * @author Anurag Munda
+ * @notice A basic smart contract to carry out staking functionalities
+ */
+contract AsvaStaking {
+    /*==============================================================
+                            STATE VARIABLES
+    ==============================================================*/
+
+    /// @dev Total amount staked currently in the pool
+    uint256 private totalPoolAmount;
+
+    /// @dev Maps a user address to their total staked amount
+    mapping(address user => uint256 amountStaked) private s_userStakedBalance;
+
+
+    /*==============================================================
+                                FUNCTIONS
+    ==============================================================*/
+
+    /**
+     * @notice Function used to stake funds
+     */
+    function stake() external {}
+
+    /**
+     * @notice Function used to unstake the staked funds
+     */
+    function unstake() external {}
+
+    /**
+     * @notice Function used to get the stake of a user
+     */
+    function getStake() external {}
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,28 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.30",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/42412892f1e54e148e2ad6fe865d0f24",
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
 
 export default config;
